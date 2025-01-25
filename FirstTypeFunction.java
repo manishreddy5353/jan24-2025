9fecc9023de74f1dbd430c8209933f67e79f6a69
manishreddy5353 <bmanish20849@gmail.com>
2025-01-25 12:19:53 +0530

    Add files via upload

---
+++ FirstTypeFunction.java
@@ -0,0 +1,27 @@
+package firstypefunction;
+
+public class FirstTypeFunction {
+	
+	static void pattern() {
+		System.out.println("");
+		System.out.println("----------");
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		pattern();
+		System.out.println("welcome");
+		
+		pattern();
+		System.out.println("hyderabad");
+		
+		pattern();
+		System.out.println("st peters engineering college");
+		
+
+	}
+
+}
